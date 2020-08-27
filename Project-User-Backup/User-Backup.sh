4a17e82022c78286739170a3afe344e879804c7f
Cloud User <centos@ip-172-31-19-187.us-east-2.compute.internal>
2020-08-27 19:55:36 +0000

    I completely updated the User-backup.sh script to include multiple compression types. I also moved the script into functions to be more organized and debug easily.

--- Project-User-Backup/User-Backup.sh
+++ Project-User-Backup/User-Backup.sh
@@ -1,48 +1,74 @@
 #!/bin/bash
 
-### Backup Script Information ###
-# This script will ask the user which $user they want to create a backup for. Then using tar with gzip compression
-# it will back up the home directory.
+# Backup Script Information #
+### This script will ask the user which $user they want to create a backup for. Then using tar with gzip compression
+### it will back up the home directory.
 
-### How it Works ###
-#	Step 1:) Ask the user for the $name of the account they want to back up
-#	Step 2:) The script then verifies that the $name exists on the system
-#	Step 3:) If the user does exist the script then verifies with the user that they want to move forward
-#	Step 4:) Then a tar script with gzip compress runs
-#	Step 5:) The backup_$name.tar.gz is then placed in /home/backup/$name, this directory needs 777 permissions
-#	Step 6:) The script then makes a copy of /home/backup/back_$name.tar.gz and places it in the users home directory
+# How it Works #
+###	Step 1:) Runs function Get_Name. This function asks the user for the $name (username) to create a backup for.
+###		 It then has a if statement to ensure that the user exists on the machine.
+###	Step 2:) Runs the funcation Get_Compression_Tpe. Which allows the user to select between gzi (gz), 
+###		 bzip2 (bz2) or none (none)
+###	Step 3:) Runs the funcation Create_Tar_compression. Which uses the tar command with the compression type
+###		choosen in previous step to create the backup
+###	Step 4:) Run the funcation Move_Backup. Which creates a copy of teh back up in the usrs home directory.
 
-### To Do ###
-#	~ Turn into a one liner
 
-### Advance To Do ###
-#	~ Push the tar.gz file newly created to a AWS ECS instance for off premise storage
-#	~ Add a function that allows the user to choose between gzip, b2zip and xz compress types.
+# To Do #
+###	~ Turn into a one liner
 
-#### Start of Script ###
+# Advance To Do #
+###	~ Push the tar.gz file newly created to a AWS ECS instance for off premise storage
 
-# Ask the user for the $name to backup
+# Start of Script #
 
-echo "Which name do you want to create a backup for?"
-read name
-name=$name
+### Ask the user for the $name to backup
 
-# If statement to ensure that the $name home directory actaully exists and can be backup.
+function Get_Name {
+	echo "Which name do you want to create a backup for?"
+	read name
+	name=$name
 
-if [ -d /home/"$name" ] ;then
-	echo "The user exist, did you want to complete the backup of user: $name? (yes/no)"
-	read answer
-	answer=$answer
-
-	if [ $answer == "yes" ] ;then
-		tar -czvf /home/backup/$name/backup_$name.tar.gz /home/centos && echo "The user $name backup is complete! It is being stored at /home/$name/backup_$name.tar.gz"
-
-else
-	echo "The user does NOT exists, ending script imcomplete!"
+if [ ! -d /home/"$name" ] ;then
+	echo "The user doesn't exist! Ending Script" && exit
 fi
+}
+
+### Ask the user for which type of compression to use gzip, b2zip or xz.
+
+function Get_Compression_Type {
+ 	echo "How did you want to compress the backup? (gz, bz2, none)"
+	read type
+	type=$type
+}
+
+### This function creates the backup with the compression choosen by the funcation Get_Compression_Type
+
+function Create_Tar_Compressed {
+	if [ $type = gz ] ;then
+		tar -czvf /home/backup/$name/backup_$name.tar.gz /home/$name
+	elif [ $type = bz2 ] ;then
+		tar -cjvf /home/backup/$name/backup_$name.tar.bz2 /home/$name
+	else
+		tar -cvf /home/backup/$name/backup_$name.1tar /home/$name
 fi
+}
 
-# Copying the backup from /home/backup/$name to /home/$name
+### Copying the backup from /home/backup/$name to /home/$name
 
-cp /home/backup/$name/backup_$name.tar.gz /home/$name
-echo "A copy of the back up has been placed in $name home directory."
+function Move_Backup {
+	cp /home/backup/$name/backup_$name.tar.$type /home/$name
+	echo "A copy of the back up has been placed in $name home directory."
+}
+
+### Calling Functions
+
+Get_Name
+Get_Compression_Type
+Create_Tar_Compressed
+Move_Backup
+
+
+
+
+
